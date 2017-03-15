47a40b483f95b465d66903670e3bfe932d36504d
annalevijeva <annalevijeva@github.com>
2017-03-15 21:08:09 +0200

    optimaze equation.equation, modify requirements

--- equation.py
+++ equation.py
@@ -20,35 +20,28 @@ def is_list_of_numbers(args):
 
 def equation(a, b, c):
     solutions = []
-    if a == 0 and b == 0 and c == 0:
-        return "True"
-    elif a == 0 and b == 0:
-        return "False"
-    elif a == 0:
-        # b*x + c = 0
-        x = c / b
-        solutions.append(x)
-        return solutions
-    elif b == 0:
-        # a * x**2 + c = 0
-        x = math.sqrt(c/a)
-        solutions.append(x)
-        solutions.append(-x)
-        return solutions
-    else:
-        D = b**2 - 4*a*c
-        if D > 0:
-            x1 = ((-b) + math.sqrt(D)) / 2*a
-            x2 = ((-b) - math.sqrt(D)) / 2*a
+    if a != 0:
+        if (b ** 2 - 4 * a * c) >= 0:
+            x1 = ((-b) + math.sqrt(b ** 2 - 4 * a * c)) / 2 * a
+            x1 = round(x1, 3)
+            x2 = ((-b) - math.sqrt(b ** 2 - 4 * a * c)) / 2 * a
+            x2 = round(x2, 3)
             solutions.append(x1)
             solutions.append(x2)
-            return solutions
-        elif D == 0:
-            x = ((-b) + math.sqrt(D)) / 2*a
-            solutions.append(x)
-            return solutions
         else:
-            return solutions
+            pass
+    else:
+        if b != 0:
+            x = - c / b
+            x = round(x, 3)
+            solutions.append(x)
+        else:
+            if c != 0:
+                pass
+    if len(solutions) == 0:
+        return "This equation has no roots"
+    else:
+        return set(solutions)
 
 
 def solve(args):
@@ -57,4 +50,4 @@ def solve(args):
     else:
         return "Incorrect input.\na, b and c must be numbers. Using float numbers separate them with '.'"
 
-print(solve([2,-1,0]))
+print(solve([0, -1, 9]))
---
+++ test_equation.py
@@ -0,0 +1,15 @@
+import unittest
+from equation import equation
+
+
+# class EquationTestCase(unittest.TestCase):
+#     def test_equation(self):
+#         result = equation(-2, 1, 1)
+#         self.assertEqual(result, equation(-2, 1, 1))
+
+class EquationTestCase(unittest.TestCase):
+    def test_solves_equation(self):
+        """Solve y=1*(x**2) + 0*x - 4"""
+        solution = [2, -2]  # or [-2, 2]
+        result = equation(1, 0, -4)
+        self.assertEqual(solution, result)
